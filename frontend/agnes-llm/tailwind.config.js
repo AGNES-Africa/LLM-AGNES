98b14a3a27a37ae09fe70e896a1dc604906b77e5
anisaragalo <anisaragalo@gmail.com>
2024-03-14 05:14:05 +0000

    tailwind.js

---
+++ frontend/agnes-llm/tailwind.config.js
@@ -0,0 +1,14 @@
+import {nextui} from "@nextui-org/react";
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class",
+  plugins: [nextui()]
+}
+
