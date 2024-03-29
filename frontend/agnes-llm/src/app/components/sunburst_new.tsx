55286b46aecadcfb45268ced0767aaa2ce9dc05c
anisaragalo <anisaragalo@gmail.com>
2024-03-29 07:01:47 +0000

    New sunburst chart from highcharts

---
+++ frontend/agnes-llm/src/app/components/sunburst_new.tsx
@@ -0,0 +1,286 @@
+"use client"
+
+import React, { useState, useEffect, useRef } from 'react';
+import HighchartsReact from 'highcharts-react-official';
+import Highcharts from 'highcharts';
+import HighchartsSunburst from 'highcharts/modules/sunburst';
+
+HighchartsSunburst(Highcharts);
+
+export default function SunburstChart(){
+    const [data, set_data] = useState([{
+        id: '0.0',
+        parent: '',
+        name: 'Streams'
+    }])
+
+    let sunburst_icon = null
+
+    const sunburstDiv = useRef(null);
+
+    useEffect(() => {
+        fetch('http://localhost:8000/api/db_hierarchy')
+          .then((res) => res.json())
+          .then((data:any) => {
+            let arr = [{
+                id: '0',
+                parent: '',
+                name: 'Climate Action Streams'
+            }]
+
+            for (let i = 0; i < data.length; i++) {
+                arr.push({
+                    id: (i+1).toString(),
+                    parent: '0',
+                    name: data[i].name
+                })
+                for (let j = 0; j < data[i].children.length; j++) {
+                    arr.push({
+                        id: (i+1) + "." + (j+1),
+                        parent: (i+1).toString(),
+                        name: ((data[i].children)[j]).name
+                    })
+
+                    for (let k = 0; k < data[i].children[j].children.length; k++) {
+                        arr.push({
+                            id: (i+1) + "." + (j+1) + "." + (k+1),
+                            parent: (i+1) + "." + (j+1),
+                            name: (((data[i].children)[j]).children[k]).name,
+                            value: 10
+                        })
+                    }
+                }
+            }
+            set_data(arr)
+        })
+    }, []);
+
+    const chartOptions = {
+        credits: {
+            enabled: false
+        },
+        visibleLevels: {
+            from: 0,
+            to: 3
+        },
+        chart: {
+            height: '50%'
+        },
+        colors: ['transparent'].concat(Highcharts.getOptions().colors),
+    
+        title: {
+            text: ''
+        },
+        levelSize: 1,
+        series: [{
+            type: 'sunburst',
+            data: data,
+            name: 'Root',
+            allowDrillToNode: true,
+            borderRadius: 3,
+            cursor: 'pointer',
+            dataLabels: {
+                format: '{point.name}',
+                filter: {
+                    property: 'innerArcLength',
+                    operator: '>',
+                    value: 16
+                }
+            },
+            point: {
+                events: {
+                    click: function(e) {
+                        var chart = this.series.chart;
+                        //console.log(e)
+                        if((e.point.id == '1') || (e.point.id == '2') || (e.point.id == '3') || (e.point.id == '4')){
+                            triggerLevel(3, chart)
+                        }
+                        else if(e.point.id.split(".").length == 2) {
+                            triggerLevel(4, chart)
+                        }
+                    }
+                }
+            },
+            levels: [{
+                level: 1,
+                levelSize: {
+                    value: 1
+                },
+                levelIsConstant: false,
+                dataLabels: {
+                    filter: {
+                        property: 'outerArcLength',
+                        operator: '>',
+                        value: 64
+                    }
+                }
+            }, {
+                level: 2,
+                colorByPoint: true,
+                levelSize: {
+                    value: 1
+                }
+        
+            }, {
+                level: 3,
+                colorVariation: {
+                    key: 'brightness',
+                    to: -0.5
+                },
+                hidden: true,
+                levelSize: {
+                    value: 0
+                },
+                dataLabels:{
+                    enabled:false
+                }
+            }, {
+                level: 4,
+                hidden: true,
+                levelSize: {
+                    value: 0
+                },
+                dataLabels:{
+                    enabled:false
+                }
+            }]
+        }],
+        tooltip: {
+            headerFormat: '',
+            pointFormat: ''
+        },
+        navigation: {
+            breadcrumbs: {
+                events: {
+                    click: function(e,br) {
+                        var chart = this.chart;
+                        //console.log(e)
+                        console.log(br.level)
+                        if(br.level == 1){
+                            showLevel(2, chart)
+                            hideLevel(3, chart)
+                            hideLevel(4, chart)
+                        }
+                        if(br.level == 2){
+                            sunburst_icon.destroy()
+                            showLevel(3, chart)
+                            hideLevel(4, chart)
+                        }
+                        if(br.level == 3){
+                            //open list
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    function onRender(chart) {
+        sunburst_icon = this.renderer.image('https://www.highcharts.com/samples/graphics/sun.png', 100, 160, 30, 30).add();
+    };
+
+    function showLevel(levelId, chart) {
+        var id = levelId 
+        var levels = chart.userOptions.series[0].levels // Whole levels object
+        var level = levels.filter(elem => { // Find level to hide
+            return elem.level === levelId
+        })
+        var index = levels.indexOf(level[0]) // Get index of level
+        var newLevelOptions;
+        
+        level[0].hidden = false // set flag
+        newLevelOptions = Object.assign({}, level[0], { // Merge specific level options
+            levelSize: {
+                value: 1
+            },
+            dataLabels: Object.assign({}, level[0].dataLabels, {
+                enabled: true
+            })
+        })
+        
+        levels[index] = newLevelOptions // Assign new options
+        
+        chart.series[0].update({ // Update chart levels
+            levels: levels
+        })
+    }
+
+    function hideLevel(levelId, chart) {
+        //console.log(levelId)
+        //console.log(chart)
+        var id = levelId // Level that you want to hide
+        var levels = chart.userOptions.series[0].levels // Whole levels object
+        var level = levels.filter(elem => { // Find level to hide
+            return elem.level === levelId
+        })
+        var index = levels.indexOf(level[0]) // Get index of level to hide
+        var newLevelOptions;
+        
+        level[0].hidden = true // set flag
+        newLevelOptions = Object.assign({}, level[0], {
+            levelSize: {
+                value: 0
+            },
+            dataLabels: Object.assign({}, level[0].dataLabels, {
+                enabled: false
+            })
+        })
+        
+        levels[index] = newLevelOptions // Assign new options
+        chart.series[0].update({ // Update chart levels
+            levels: levels
+        })
+    }
+      
+    function triggerLevel(levelId, chart, alternate=true) {
+        //console.log(levelId)
+        //console.log(chart)
+        var id = levelId // Level that you want to hide
+        var levels = chart.userOptions.series[0].levels // Whole levels object
+        var level = levels.filter(elem => { // Find level to hide
+            return elem.level === levelId
+        })
+        var index = levels.indexOf(level[0]) // Get index of level to hide
+        var newLevelOptions;
+        
+        if (level[0].hidden) { // if level is hidden
+            level[0].hidden = false // set flag
+            newLevelOptions = Object.assign({}, level[0], { // Merge specific level options
+                levelSize: {
+                    value: 1
+                },
+                dataLabels: Object.assign({}, level[0].dataLabels, {
+                    enabled: true
+                })
+            })
+        } else { // If level is visible
+            if (alternate){
+                level[0].hidden = true // set flag
+                newLevelOptions = Object.assign({}, level[0], {
+                    levelSize: {
+                        value: 0
+                    },
+                    dataLabels: Object.assign({}, level[0].dataLabels, {
+                        enabled: false
+                    })
+                })
+            }
+        }
+        
+        levels[index] = newLevelOptions // Assign new options
+        chart.series[0].update({ // Update chart levels
+            levels: levels
+        })
+    }
+
+    return (
+          <div>
+            <HighchartsReact
+              highcharts={Highcharts}
+              options={chartOptions}
+              callback = {onRender}
+            />
+          </div>
+    )
+
+}
