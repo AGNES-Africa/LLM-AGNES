5bd2fa1abc7622c57442f2a3030f8c81fde6b1b4
anisaragalo <anisaragalo@gmail.com>
2024-04-07 08:16:22 +0000

    Update to list view

--- frontend/agnes-llm/src/app/category/[stream_id]/[category_id]/page.tsx
+++ frontend/agnes-llm/src/app/category/[stream_id]/[category_id]/page.tsx
@@ -3,14 +3,11 @@ import ArticleListTable from "../../../components/ArticleListTable";
 
 export default function ArticleList({params}:any) {
   return(
-    <section className="py-24">
-      <div className="container">
-        <h1 className="text-3xl font-bold">Article List</h1>
-        <ArticleListTable
-          stream_id={params.stream_id}
-          category_id={params.category_id}
-        />
-      </div>
-    </section>
+    <div className="mt-1">
+      <ArticleListTable
+        stream_id={params.stream_id}
+        category_id={params.category_id}
+      />
+    </div>
   )
 }
