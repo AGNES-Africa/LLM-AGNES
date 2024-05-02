312d3b7c010a8bdf65c9fe3e45aea2b5092573f5
anisaragalo <anisaragalo@gmail.com>
2024-05-02 18:54:51 +0000

    Bringing tsparticles into frontend

--- frontend_new/langchain-nextjs-template/app/layout.tsx
+++ frontend_new/langchain-nextjs-template/app/layout.tsx
@@ -1,11 +1,16 @@
+"use client";
+
 import "./globals.css";
 import { Public_Sans } from "next/font/google";
 import {Providers} from "./providers";
 import NavBar from "@/components/NavBar";
 import Starfield from "@/components/AnimatedBackground";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, Row, Col } from "reactstrap";
+import { Container, Row} from "reactstrap";
 
+import React, {useEffect, useState} from "react";
+import Particles, {initParticlesEngine} from "@tsparticles/react";
+import {loadFull} from "tsparticles";
 const publicSans = Public_Sans({ subsets: ["latin"] });
 
 export default function RootLayout({
@@ -13,44 +18,82 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const [init, setInit] = useState(false);
+
+  useEffect(() => {
+      if (init) {
+          return;
+      }
+
+      initParticlesEngine(async (engine) => {
+          await loadFull(engine);
+      }).then(() => {
+          setInit(true);
+      });
+  }, []);
+
   return (
     <html lang="en">
       <head>
         <title>Climate Action AI</title>
         <link rel="shortcut icon" href="llm_logo.png" />
-        <meta
-          name="description"
-          content="Starter template showing how to use LangChain in Next.js projects. See source code and deploy your own at https://github.com/langchain-ai/langchain-nextjs-template!"
-        />
-        <meta property="og:title" content="LangChain + Next.js Template" />
-        <meta
-          property="og:description"
-          content="Starter template showing how to use LangChain in Next.js projects. See source code and deploy your own at https://github.com/langchain-ai/langchain-nextjs-template!"
-        />
-        <meta property="og:image" content="/images/og-image.png" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="LangChain + Next.js Template" />
-        <meta
-          name="twitter:description"
-          content="Starter template showing how to use LangChain in Next.js projects. See source code and deploy your own at https://github.com/langchain-ai/langchain-nextjs-template!"
-        />
-        <meta name="twitter:image" content="/images/og-image.png" />
       </head>
-      <body className="light bg-gradient-to-tr from-black to-blue-300 text-white shadow-lg">
+      <body>
       <Providers>
       <Container>
-      <Starfield
-        starCount={1000}
-        starColor={[255, 255, 255]}
-        speedFactor={0.05}
-        backgroundColor="black"
-      />
+      <div className="gray">
+        {init && <Particles options={
+          {
+            "background": {
+              "color": "#282c34"
+            },
+            "particles": {
+              "color": {
+                "value": "#ffffff"
+              },
+              "links": {
+                "color": "#ffffff",
+                "distance": 150,
+                "enable": true,
+                "opacity": 0.09,
+                "width": 1
+              },
+              "collisions": {
+                "enable": true
+              },
+              "move": {
+                "enable": true,
+                "outModes": "bounce",
+                "random": false,
+                "speed": 6,
+                "straight": false
+              },
+              "number": {
+                "density": {
+                  "enable": true
+                },
+                "value": 80
+              },
+              "opacity": {
+                "value": 0.09
+              },
+              "shape": {
+                "type": "circle"
+              },
+              "size": {
+                "value": 5
+              }
+            }
+          }
+          
+        }/>}
         <Row>
           <NavBar/>
         </Row>
         <Row className="mt-2">
           {children}
         </Row>
+      </div>
       </Container>
       </Providers>
       </body>
