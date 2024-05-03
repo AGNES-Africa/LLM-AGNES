8ac161b6486b131d46e462c63ccee4252d913601
anisaragalo <anisaragalo@gmail.com>
2024-05-03 15:33:17 +0000

    Changing class name

--- frontend_new/langchain-nextjs-template/app/page.tsx
+++ frontend_new/langchain-nextjs-template/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Home() {
   const InfoCard = (
-    <div className="p-4 md:p-8 rounded bg-[#25252d] w-full max-h-[85%] overflow-hidden light2">
+    <div className="p-4 md:p-8 rounded bg-[#25252d] w-full max-h-[85%] overflow-hidden gray2">
        <Image
           src="/agnes-logo.png"
           alt="Agnes Logo"
