01360e100e34fe92f94882704f4cdd5b6ce8f093
anisaragalo <anisaragalo@gmail.com>
2024-05-10 14:54:55 +0000

    Removing particles animated background

--- frontend_new/agnes-llm/app/layout.tsx
+++ frontend_new/agnes-llm/app/layout.tsx
@@ -7,10 +7,6 @@ import NavBar from "@/components/NavBar";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Row} from "reactstrap";
 
-import React, {useEffect, useState} from "react";
-import Particles, {initParticlesEngine} from "@tsparticles/react";
-import {loadFull} from "tsparticles";
-
 const publicSans = Public_Sans({ subsets: ["latin"] });
 
 export default function RootLayout({
@@ -18,20 +14,6 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const [init, setInit] = useState(false);
-
-  useEffect(() => {
-      if (init) {
-          return;
-      }
-
-      initParticlesEngine(async (engine) => {
-          await loadFull(engine);
-      }).then(() => {
-          setInit(true);
-      });
-  }, []);
-
   return (
     <html lang="en">
       <head>
@@ -41,56 +23,6 @@ export default function RootLayout({
       <body>
       <Providers>
       <Container>
-        {init && <Particles options={
-          {
-            "background": {
-              "color": "#282c34",
-              "size" : "cover"
-            },
-            "fullScreen": {
-              "enable": true,
-              "zIndex": 1
-            },
-            "particles": {
-              "color": {
-                "value": "#ffffff"
-              },
-              "links": {
-                "color": "#ffffff",
-                "distance": 150,
-                "enable": true,
-                "opacity": 0.29,
-                "width": 1
-              },
-              "collisions": {
-                "enable": true
-              },
-              "move": {
-                "enable": true,
-                "outModes": "bounce",
-                "random": false,
-                "speed": 6,
-                "straight": false
-              },
-              "number": {
-                "density": {
-                  "enable": true
-                },
-                "value": 80
-              },
-              "opacity": {
-                "value": 0.09
-              },
-              "shape": {
-                "type": "triangle"
-              },
-              "size": {
-                "value": 5
-              }
-            }
-          }
-          
-        }/>}
         <Row>
           <NavBar/>
         </Row>
