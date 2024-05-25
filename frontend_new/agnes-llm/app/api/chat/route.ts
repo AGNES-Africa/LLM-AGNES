b63413cdb7c491d21a78087c7bdc22a84d885a72
anisaragalo <anisaragalo@gmail.com>
2024-05-25 15:49:27 +0000

    Basic llm setup

--- frontend_new/agnes-llm/app/api/chat/route.ts
+++ frontend_new/agnes-llm/app/api/chat/route.ts
@@ -1,72 +1,185 @@
 import { NextRequest, NextResponse } from "next/server";
 import { Message as VercelChatMessage, StreamingTextResponse } from "ai";
 
-import { ChatOpenAI } from "@langchain/openai";
-import { PromptTemplate } from "@langchain/core/prompts";
-import { HttpResponseOutputParser } from "langchain/output_parsers";
+import {
+  DistanceStrategy,
+  PGVectorStore,
+} from "@langchain/community/vectorstores/pgvector";
+import { PoolConfig } from "pg";
 
-export const runtime = "edge";
+import { AIMessage, ChatMessage, HumanMessage } from "@langchain/core/messages";
+import { ChatOpenAI, OpenAIEmbeddings } from "@langchain/openai";
+import { createRetrieverTool } from "langchain/tools/retriever";
+import { AgentExecutor, createToolCallingAgent } from "langchain/agents";
 
-const formatMessage = (message: VercelChatMessage) => {
-  return `${message.role}: ${message.content}`;
+import {
+  ChatPromptTemplate,
+  MessagesPlaceholder,
+} from "@langchain/core/prompts";
+
+export const runtime = "nodejs";
+
+const convertVercelMessageToLangChainMessage = (message: VercelChatMessage) => {
+  if (message.role === "user") {
+    return new HumanMessage(message.content);
+  } else if (message.role === "assistant") {
+    return new AIMessage(message.content);
+  } else {
+    return new ChatMessage(message.content, message.role);
+  }
 };
 
-const TEMPLATE = `You are a climate change expert. All responses must be accurate.
-
-Current conversation:
-{chat_history}
-
-User: {input}
-AI:`;
+const AGENT_SYSTEM_TEMPLATE = `You are an expert on climate change negotiations. All responses must be accurate. Use the available tools to look up the answers to all questions.`;
 
 /**
- * This handler initializes and calls a simple chain with a prompt,
- * chat model, and output parser. See the docs for more information:
+ * This handler initializes and calls a retrieval agent. It requires an OpenAI
+ * Functions model. See the docs for more information:
  *
- * https://js.langchain.com/docs/guides/expression_language/cookbook#prompttemplate--llm--outputparser
+ * https://js.langchain.com/docs/use_cases/question_answering/conversational_retrieval_agents
  */
 export async function POST(req: NextRequest) {
   try {
     const body = await req.json();
-    const messages = body.messages ?? [];
-    const formattedPreviousMessages = messages.slice(0, -1).map(formatMessage);
+    /**
+     * We represent intermediate steps as system messages for display purposes,
+     * but don't want them in the chat history.
+     */
+    const messages = (body.messages ?? []).filter(
+      (message: VercelChatMessage) =>
+        message.role === "user" || message.role === "assistant",
+    );
+    const returnIntermediateSteps = false;
+    const previousMessages = messages
+      .slice(0, -1)
+      .map(convertVercelMessageToLangChainMessage);
     const currentMessageContent = messages[messages.length - 1].content;
-    const prompt = PromptTemplate.fromTemplate(TEMPLATE);
 
-    /**
-     * You can also try e.g.:
-     *
-     * import { ChatAnthropic } from "langchain/chat_models/anthropic";
-     * const model = new ChatAnthropic({});
-     *
-     * See a full list of supported models at:
-     * https://js.langchain.com/docs/modules/model_io/models/
-     */
-    const model = new ChatOpenAI({
-      temperature: 0.8,
+    const chatModel = new ChatOpenAI({
       modelName: "gpt-3.5-turbo-1106",
+      temperature: 0.2,
+      // IMPORTANT: Must "streaming: true" on OpenAI to enable final output streaming below.
+      streaming: true,
+    });
+   
+    const config = {
+      postgresConnectionOptions: {
+        type: "postgres",
+        host: process.env.HOST_NAME,
+        port: 5432,
+        user: process.env.USER_NAME,
+        password: process.env.PASSWORD,
+        database: process.env.DB_NAME,
+        ssl: true
+      } as PoolConfig,
+      schemaName: "embed",
+      tableName: "document_embeddings",
+      columns: {
+        idColumnName: "id",
+        vectorColumnName: "vector",
+        contentColumnName: "content",
+        metadataColumnName: "document_id",
+      },
+      // supported distance strategies: cosine (default), innerProduct, or euclidean
+      distanceStrategy: "cosine" as DistanceStrategy
+    };
+
+    const vectorstore = new PGVectorStore(
+      new OpenAIEmbeddings({apiKey: process.env.OPENAI_API_KEY, model: "text-embedding-3-small"}),
+      config
+    );
+
+    const retriever = vectorstore.asRetriever();
+    
+    /**
+     * Wrap the retriever in a tool to present it to the agent in a
+     * usable form.
+     */
+    const tool = createRetrieverTool(retriever, {
+      name: "llm_documents",
+      description: "Searches and returns information on climate change negotiation streams",
     });
 
     /**
-     * Chat models stream message chunks rather than bytes, so this
-     * output parser handles serialization and byte-encoding.
-     */
-    const outputParser = new HttpResponseOutputParser();
-
-    /**
-     * Can also initialize as:
+     * Based on https://smith.langchain.com/hub/hwchase17/openai-functions-agent
      *
-     * import { RunnableSequence } from "@langchain/core/runnables";
-     * const chain = RunnableSequence.from([prompt, model, outputParser]);
+     * This default prompt for the OpenAI functions agent has a placeholder
+     * where chat messages get inserted as "chat_history".
+     *
+     * You can customize this prompt yourself!
      */
-    const chain = prompt.pipe(model).pipe(outputParser);
+    const prompt = ChatPromptTemplate.fromMessages([
+      ["system", AGENT_SYSTEM_TEMPLATE],
+      new MessagesPlaceholder("chat_history"),
+      ["human", "{input}"],
+      new MessagesPlaceholder("agent_scratchpad"),
+    ]);
 
-    const stream = await chain.stream({
-      chat_history: formattedPreviousMessages.join("\n"),
-      input: currentMessageContent,
+    const agent = await createToolCallingAgent({
+      llm: chatModel,
+      tools: [tool],
+      prompt,
     });
 
-    return new StreamingTextResponse(stream);
+    const agentExecutor = new AgentExecutor({
+      agent,
+      tools: [tool],
+      // Set this if you want to receive all intermediate steps in the output of .invoke().
+      returnIntermediateSteps: true,
+    });
+
+    if (!returnIntermediateSteps) {
+      /**
+       * Agent executors also allow you to stream back all generated tokens and steps
+       * from their runs.
+       *
+       * This contains a lot of data, so we do some filtering of the generated log chunks
+       * and only stream back the final response.
+       *
+       * This filtering is easiest with the OpenAI functions or tools agents, since final outputs
+       * are log chunk values from the model that contain a string instead of a function call object.
+       *
+       * See: https://js.langchain.com/docs/modules/agents/how_to/streaming#streaming-tokens
+       */
+      const logStream = await agentExecutor.streamLog({
+        input: currentMessageContent,
+        chat_history: previousMessages,
+      });
+
+      const textEncoder = new TextEncoder();
+      const transformStream = new ReadableStream({
+        async start(controller) {
+          for await (const chunk of logStream) {
+            if (chunk.ops?.length > 0 && chunk.ops[0].op === "add") {
+              const addOp = chunk.ops[0];
+              if (
+                addOp.path.startsWith("/logs/ChatOpenAI") &&
+                typeof addOp.value === "string" &&
+                addOp.value.length
+              ) {
+                controller.enqueue(textEncoder.encode(addOp.value));
+              }
+            }
+          }
+          controller.close();
+        },
+      });
+
+      return new StreamingTextResponse(transformStream);
+    } else {
+      /**
+       * Intermediate steps are the default outputs with the executor's `.stream()` method.
+       * We could also pick them out from `streamLog` chunks.
+       * They are generated as JSON objects, so streaming them is a bit more complicated.
+       */
+      const result = await agentExecutor.invoke({
+        input: currentMessageContent,
+        chat_history: previousMessages,
+      });
+      return NextResponse.json(
+        { output: result.output, intermediate_steps: result.intermediateSteps },
+        { status: 200 },
+      );
+    }
   } catch (e: any) {
     return NextResponse.json({ error: e.message }, { status: e.status ?? 500 });
   }
