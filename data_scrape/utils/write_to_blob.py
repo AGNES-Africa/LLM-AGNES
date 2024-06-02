147e27196995c6f08a41d9ed93dc7c589dbaf132
anisaragalo <anisaragalo@gmail.com>
2024-06-02 06:36:25 +0000

    Adding logic to write raw text files to the blob

--- data_scrape/utils/write_to_blob.py
+++ data_scrape/utils/write_to_blob.py
@@ -1,6 +1,7 @@
 import pdfplumber
 import os
 import io
+import re
 import requests
 from azure.storage.blob import BlobServiceClient, ContentSettings
 from dotenv import load_dotenv
@@ -15,6 +16,23 @@ headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/
 connection_string = os.getenv('BLOB_CONNECTION_STRING')
 blob_service_client = BlobServiceClient.from_connection_string(connection_string)
 container_name = os.getenv('BLOB_CONTAINER_NAME')
+
+def sanitise_metadata(metadata):
+    """
+    Sanitise metadata by removing illegal characters.
+    """
+    custom_weights = "-!#$%&*.^_|~+\"\'(),/`~0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[]abcdefghijklmnopqrstuvwxyz{} "
+    
+    def remove_illegal_chars(s):
+        # Remove characters not in custom_weights
+        s = ''.join(c for c in s if c in custom_weights or c == ' ')
+        # Remove control characters
+        s = re.sub(r'[\x00-\x1F\x7F-\x9F]', '', s)
+        return s[:255]  # Truncate to max length
+
+    sanitised_metadata = {remove_illegal_chars(key): remove_illegal_chars(str(value)) for key, value in metadata.items()}
+    return sanitised_metadata
+
 
 def upload_file_to_blob(entry, negotiation_stream, source, category_name):
     """ 
@@ -38,7 +56,7 @@ def upload_file_to_blob(entry, negotiation_stream, source, category_name):
 
     blob_client = blob_service_client.get_blob_client(container_name, blob_name)
     
-        # Upload the PDF as a blob
+    # Upload the PDF as a blob
     content_settings = ContentSettings(content_type='application/pdf')
     blob_client.upload_blob(pdf_data, overwrite=True, content_settings=content_settings)
     print(f"File uploaded to {blob_name} in container {container_name}")
@@ -48,5 +66,13 @@ def upload_file_to_blob(entry, negotiation_stream, source, category_name):
         text = ''
         for page in pdf.pages:
             text += page.extract_text()
+
+        blob_client_txt = blob_service_client.get_blob_client(container_name, f"{blob_name}.txt")
+        text_metadata = {
+            'URL': entry['url'],
+            'Summary': entry.get('summary', '')
+        }
+        text_sanitised_metadata = sanitise_metadata(text_metadata)
+        blob_client_txt.upload_blob(text, metadata=text_sanitised_metadata, overwrite=True)
 
     return entry, text
