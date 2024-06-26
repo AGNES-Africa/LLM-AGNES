edb9753f5b055260fbc620507ddd3af5598a08ba
anisaragalo <anisaragalo@gmail.com>
2024-06-26 21:50:36 +0000

    Custom retriever with ability to search unfccc decision titles as well as unfccc proceedings

--- frontend_new/agnes-llm/app/api/chat/route.ts
+++ frontend_new/agnes-llm/app/api/chat/route.ts
@@ -1,6 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
 import { Message as VercelChatMessage } from "ai";
-import { RunnableBranch } from "@langchain/core/runnables";
 import { StringOutputParser } from "@langchain/core/output_parsers"
 
 import {
@@ -21,7 +20,144 @@ import type { BaseMessage } from "@langchain/core/messages";
 import {
   RunnablePassthrough,
   RunnableSequence,
+  RunnableBranch
 } from "@langchain/core/runnables";
+
+import {
+  BaseRetriever,
+  type BaseRetrieverInput,
+} from "@langchain/core/retrievers";
+import type { CallbackManagerForRetrieverRun } from "@langchain/core/callbacks/manager";
+import { Document } from "@langchain/core/documents";
+
+import {removeStopwords, eng}  from "stopword";
+import {compareTwoStrings} from "string-similarity";
+
+interface CustomRetrieverInput extends BaseRetrieverInput {}
+const additional_stop_words = ["decision","decisions","latest","recent","main","key","point","points","idea","ideas"];
+const postgresOptions = {
+  type: "postgres",
+  host: process.env.HOST_NAME,
+  port: 5432,
+  user: process.env.USER_NAME,
+  password: process.env.PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: true
+}
+
+class CustomRetriever extends BaseRetriever {
+  lc_namespace = ["langchain", "retrievers"];
+
+  constructor(fields?: CustomRetrieverInput) {
+    super(fields);
+  }
+
+  async _getRelevantDocuments(
+    query: string,
+    runManager?: CallbackManagerForRetrieverRun
+  ): Promise<Document[]> {
+
+    const punctRE = /[\u2000-\u206F\u2E00-\u2E7F\\'!"#$%&()*+,\-.\/:;<=>?@\[\]^_`{|}~]/g;
+    query = query.replace(punctRE, '');
+  
+    const config = {
+      postgresConnectionOptions: postgresOptions as PoolConfig,
+      schemaName: "embed",
+      tableName: "vw_decisions_documents",
+      columns: {
+        idColumnName: "id",
+        vectorColumnName: "title_vector",
+        contentColumnName: "title",
+        metadataColumnName: "metadata",
+      },
+      distanceStrategy: "cosine" as DistanceStrategy
+    };
+  
+    const vectorstore = new PGVectorStore(
+      new OpenAIEmbeddings({apiKey: process.env.OPENAI_API_KEY, model: "text-embedding-3-small"}),
+      config
+    );
+
+    const retriever = vectorstore.asRetriever();
+
+    const query_mod = removeStopwords(query.split(" "),eng.concat(additional_stop_words)).join(" ").toLowerCase();
+
+    let docs = await retriever._getRelevantDocuments(query_mod);
+
+    let metadata_filtered = []
+    for (let i in docs) {
+      let pageContent = docs[i].pageContent.split("-")[1]
+      let metadata = docs[i].metadata
+      let id = parseInt(metadata["id"])
+      let created = metadata["created"]
+      let pageContent_mod = removeStopwords(pageContent.split(" "), eng.concat(additional_stop_words)).join(" ").toLowerCase() 
+      let similarity_score = compareTwoStrings(query_mod, pageContent_mod)
+      if ( similarity_score > 0.5 ){
+        metadata_filtered.push({"id":id,"created":created})
+      }
+    }
+    metadata_filtered.sort(function(a,b){
+      return new Date(b.created).getTime() - new Date(a.created).getTime();
+    });
+  
+    let document_ids = []
+    metadata_filtered = metadata_filtered.slice(0,2)
+    for (let i in metadata_filtered) {
+      document_ids.push(metadata_filtered[i].id)
+    }
+
+    let decisions_docs = [];
+
+    if(document_ids.length > 0){
+      const config2 = {
+        postgresConnectionOptions: postgresOptions as PoolConfig,
+        schemaName: "embed",
+        tableName: "vw_decisions_embeddings",
+        columns: {
+          idColumnName: "id",
+          vectorColumnName: "vector",
+          contentColumnName: "content",
+          metadataColumnName: "metadata",
+        },
+        distanceStrategy: "cosine" as DistanceStrategy
+      };
+
+      const vectorstore2 = new PGVectorStore(
+        new OpenAIEmbeddings({apiKey: process.env.OPENAI_API_KEY, model: "text-embedding-3-small"}),
+        config2
+      );
+    
+      const retriever2 = vectorstore2.asRetriever({filter:{document_id:{"in":document_ids}}});
+      decisions_docs = await retriever2._getRelevantDocuments(query);
+    }
+    
+    const config3 = {
+      postgresConnectionOptions: postgresOptions as PoolConfig,
+      schemaName: "embed",
+      tableName: "vw_proceedings_embeddings",
+      columns: {
+        idColumnName: "id",
+        vectorColumnName: "vector",
+        contentColumnName: "content",
+        metadataColumnName: "metadata",
+      },
+      distanceStrategy: "cosine" as DistanceStrategy
+    };
+
+    const vectorstore3 = new PGVectorStore(
+      new OpenAIEmbeddings({apiKey: process.env.OPENAI_API_KEY, model: "text-embedding-3-small"}),
+      config3
+    );
+  
+    const retriever3 = vectorstore3.asRetriever();
+    let proceedings_docs = await retriever3._getRelevantDocuments(query);
+
+    if(decisions_docs.length > 0){
+      return decisions_docs
+    }
+    return decisions_docs.concat(proceedings_docs);
+  }
+}
 
 export const runtime = "nodejs";
 
@@ -35,7 +171,10 @@ const convertVercelMessageToLangChainMessage = (message: VercelChatMessage) => {
   }
 };
 
-const AGENT_SYSTEM_TEMPLATE = `You are an expert on climate change negotiations. You should only answer questions based on the context below. Don't make up answers. If you do not know the answer, ask the user to rephrase his/her question.
+const AGENT_SYSTEM_TEMPLATE = `You are an expert on climate change negotiations. 
+Answer the user's questions based on the below context. 
+If the context doesn't contain any relevant information to the question, don't make something up and ask the user to rephrase his/her question:
+
 <context>
 {context}
 </context>
@@ -61,36 +200,11 @@ export async function POST(req: NextRequest) {
     const previousMessages = messages
       .slice(0, -1)
       .map(convertVercelMessageToLangChainMessage);
+
     const currentMessageContent = messages[messages.length - 1].content;
-   
-    const config = {
-      postgresConnectionOptions: {
-        type: "postgres",
-        host: process.env.HOST_NAME,
-        port: 5432,
-        user: process.env.USER_NAME,
-        password: process.env.PASSWORD,
-        database: process.env.DB_NAME,
-        ssl: true
-      } as PoolConfig,
-      schemaName: "embed",
-      tableName: "llm_documents_with_metadata",
-      columns: {
-        idColumnName: "id",
-        vectorColumnName: "vector",
-        contentColumnName: "content",
-        metadataColumnName: "url",
-      },
-      // supported distance strategies: cosine (default), innerProduct, or euclidean
-      distanceStrategy: "cosine" as DistanceStrategy
-    };
 
-    const vectorstore = new PGVectorStore(
-      new OpenAIEmbeddings({apiKey: process.env.OPENAI_API_KEY, model: "text-embedding-3-small"}),
-      config
-    );
-
-    const retriever = vectorstore.asRetriever();
+    //const retriever = vectorstore.asRetriever();
+    const retriever = new CustomRetriever({});
    
     const prompt = ChatPromptTemplate.fromMessages([
       ["system", AGENT_SYSTEM_TEMPLATE],
@@ -139,18 +253,30 @@ export async function POST(req: NextRequest) {
     });
 
     let res = await conversationalRetrievalChain.invoke({
-      input: [
+      input: previousMessages.concat([
         new HumanMessage(currentMessageContent),
-      ],
+      ]),
     });
 
     let sources:any = [];
 
-    if((!res.answer.toLowerCase().includes("if you have any questions")) && (!res.answer.includes("rephrase")) && (!res.answer.includes("cannot provide")) && (!res.answer.includes("can't provide"))&& (!res.answer.includes("sorry"))&& (!res.answer.toLowerCase().includes("feel free"))){
+    if(
+      (!res.answer.toLowerCase().includes("if you have any questions")) && 
+      (!res.answer.toLowerCase().includes("do you have")) && 
+      (!res.answer.toLowerCase().includes("here to help")) && 
+      (!res.answer.toLowerCase().includes("here to provide")) && 
+      (!res.answer.toLowerCase().includes("rephrase")) && 
+      (!res.answer.toLowerCase().includes("cannot provide")) && 
+      (!res.answer.toLowerCase().includes("can't provide")) &&
+      (!res.answer.toLowerCase().includes("please provide")) && 
+      (!res.answer.toLowerCase().includes("sorry")) &&
+      (!res.answer.toLowerCase().includes("i can help")) &&
+      (!res.answer.toLowerCase().includes("feel free"))
+    ){
       if (res.context){
         if(res.context.length > 0){
           for (let i = 0; i < res.context.length; i++) {
-            let metadata = res.context[i].metadata
+            let metadata = res.context[i].metadata.url
             if(!sources.includes(metadata)){
               sources.push(metadata)
             }
